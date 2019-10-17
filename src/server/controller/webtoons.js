47d95af1153877c857d319a600f78c113778e776
unknown <iam.mhd07@gmail.com>
2019-10-17 12:18:36 +0700

    add search webtoon implementation

--- src/server/controller/webtoons.js
+++ src/server/controller/webtoons.js
@@ -1,7 +1,17 @@
 const models = require('../models');
+const Sequelize = require('sequelize');
 
+const Op = Sequelize.Op;
 const masters = models.master;
 
 exports.index = (req, res) => {
   masters.findAll().then(result => res.send(result));
+};
+
+exports.ShowSearchWebtoon = (req, res) => {
+  const cari = req.query.title;
+  console.log(cari);
+  masters.findAll({
+    where: { title: { [Op.like]: `%${cari}%` } }
+  }).then(result => res.send(result)); 
 };
